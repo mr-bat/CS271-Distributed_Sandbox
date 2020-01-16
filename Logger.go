f9cdf4baa75f2adca5ed7a349c996faf2820260d
Abtin Bateni <abtinbateni@gmail.com>
2020-01-15 19:16:33 -0800

    add logger

--- Logger.go
+++ Logger.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var Logger = logrus.New()
+
+func initLogger()  {
+	Logger.Formatter.(*logrus.TextFormatter).DisableTimestamp = true // remove timestamp from test output
+	Logger.Level = logrus.InfoLevel
+	Logger.Out = os.Stdout
+}
