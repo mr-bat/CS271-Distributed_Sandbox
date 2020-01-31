e8a336d354c5ce713cee69b56ffee09b500d3922
Abtin Bateni <abtinbateni@gmail.com>
2020-01-31 12:39:29 -0800

    add blockchain parser

--- Blockchain_test.go
+++ Blockchain_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -44,6 +45,31 @@ func TestBlockchain(t *testing.T) {
 				t.Errorf("Balance %s was incorrect, got: %d, want: %d.", users[i], getBalance(users[i]), expectedBalances[i])
 			}
 		}
+	}
+}
+
+func TestBlockchainParser(t *testing.T) {
+	blocks := []Block{
+		{"1", "2", 1},
+		{"1", "3", 1},
+	}
+
+	if "1&2&1" != blocks[0].toString() {
+		t.Error("Incorrect conversion from single block to string")
+	}
+	if blocks[0] != parseBlock("1&2&1") {
+		t.Error("Incorrect conversion from string to single block")
+	}
+
+	addBlock("1", "2", 1)
+	addBlock("1", "3", 1)
+
+	convertedBlocks := rangeToString(0)
+	if convertedBlocks != blocks[0].toString() + blocks[1].toString() {
+		t.Error("Incorrect conversion from range of blocks to string")
+	}
+	if reflect.DeepEqual(blocks, parseRange(convertedBlocks)) {
+		t.Error("Incorrect back and forth conversion of range of blocks")
 	}
 }
 
